3b1cd5c5832cf2c12e8b428206c5367b7befe524
vanhalenar <thalenar@gmail.com>
2026-05-04 17:37:30 +0200

    updated files

--- midi.py
+++ midi.py
@@ -1,15 +1,46 @@
 import mido
 import time
 import subprocess
+import re
+import sys
+import os
 
 NAM_INPUT = "/run/jalv-nam-input"
 IR_INPUT = "/run/jalv-ir-input"
 PATTERN = "MIDI-FOOTSWITCH"
 
+NAM_PRESETS_BANK = ["urn:nam:orange-clean", "urn:nam:orange-hi-gain", "urn:nam:marshall-clean"]
+
+CURRENT_NAM_PRESET_INDEX = 0
+
+NAM_PRESET_IR_MAP = {
+    "urn:nam:orange-clean": "urn:brummer:impulseloader-state-orange",
+    "urn:nam:orange-hi-gain": "urn:brummer:impulseloader-state-orange",
+    "urn:nam:marshall-clean": "urn:brummer:impulseloader-state-marshall"
+}
+
+NAM_PRESET_OUTPUT_LEVEL_MAP = {
+    "urn:nam:orange-clean": 0,
+    "urn:nam:orange-hi-gain": -2,
+    "urn:nam:marshall-clean": 0,
+}
+
+# urn:nam:orange-clean => orange-clean
+def strip_uri(uri):
+    return re.split(':', uri)[2]
+
+def set_nam_output_level(level):
+    with open(NAM_INPUT, 'w') as f:
+        f.write(f'set output_level {level}\n')
+
 def set_nam_preset(uri):
+    ir_preset = NAM_PRESET_IR_MAP[uri]
     with open(NAM_INPUT, 'w') as f:
         f.write(f'preset {uri}\n')
 
+def set_ir_preset(uri):
+    with open(IR_INPUT, 'w') as f:
+        f.write(f'preset {uri}\n')
 
 def find_matching_port(ports):
     for p in ports:
@@ -25,6 +56,17 @@ def get_output_port_name():
     outputs = mido.get_output_names()
     return(find_matching_port(outputs))
 
+def set_preset_by_index(index):
+    nam_preset_uri = NAM_PRESETS_BANK[index]
+    ir_preset_uri = NAM_PRESET_IR_MAP[nam_preset_uri]
+    nam_preset_output_level = NAM_PRESET_OUTPUT_LEVEL_MAP[nam_preset_uri]
+    nam_preset_name = strip_uri(nam_preset_uri)
+
+    set_nam_preset(nam_preset_uri)
+    set_ir_preset(ir_preset_uri)
+    set_nam_output_level(nam_preset_output_level)
+    msg = mido.Message('sysex', data=bytearray(nam_preset_name.encode('utf-8')))
+    output_port.send(msg)
 
 def button_1_pressed(output_port):
     set_nam_preset("urn:nam:default-state")
@@ -45,11 +87,11 @@ def button_3_pressed(output_port):
     output_port.send(msg)
     print("button 3")
 
-
+# previous preset
 def button_4_pressed(output_port):
-    msg = mido.Message('sysex', data=bytearray(b'DEF'))
-    output_port.send(msg)
-    print("button 4")
+    global CURRENT_NAM_PRESET_INDEX
+    CURRENT_NAM_PRESET_INDEX = (CURRENT_NAM_PRESET_INDEX - 1) % len(NAM_PRESETS_BANK)
+    set_preset_by_index(CURRENT_NAM_PRESET_INDEX)
 
 
 def button_5_pressed(output_port):
@@ -57,15 +99,14 @@ def button_5_pressed(output_port):
     output_port.send(msg)
     print("button 5")
 
-
+# next preset
 def button_6_pressed(output_port):
-    msg = mido.Message('sysex', data=bytearray(b'FGH'))
-    output_port.send(msg)
-    print("button 6")
+    global CURRENT_NAM_PRESET_INDEX
+    CURRENT_NAM_PRESET_INDEX = (CURRENT_NAM_PRESET_INDEX + 1) % len(NAM_PRESETS_BANK)
+    set_preset_by_index(CURRENT_NAM_PRESET_INDEX) 
     
 input_port_name = get_input_port_name()
 output_port_name = get_output_port_name()
-
 
 with mido.open_input(input_port_name) as input_port, mido.open_output(output_port_name) as output_port:
     print("port opened")
@@ -79,4 +120,3 @@ with mido.open_input(input_port_name) as input_port, mido.open_output(output_por
                 case 5: button_5_pressed(output_port)
                 case 6: button_6_pressed(output_port)
 
-print("got here at least")
--- start-ir.sh
+++ start-ir.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
-# Keep write end of FIFO open so jalv stdin doesn't block
-#jalv -i ~/.lv2/nam-state-fender.lv2
-# Open write end in background to unblock the read end
 tail -f /dev/null > /run/jalv-ir-input &
 TAIL_PID=$!
 
-# Now open jalv with the FIFO as stdin
-/usr/local/bin/jalv ~/.lv2/impulseloader-state.lv2 < /run/jalv-ir-input
+/usr/bin/jalv urn:brummer:ImpulseLoader < /run/jalv-ir-input &
+JALV_PID=$!
 
-# Clean up tail when jalv exits
+until jack_lsp 2>/dev/null | grep -q "ImpulseLoader"; do
+    sleep 0.1
+done
+
+sleep 1
+
+# Use the same file descriptor, don't open/close it
+exec 3>/run/jalv-ir-input
+echo "preset urn:brummer:impulseloader-state-orange" >&3
+
+wait $JALV_PID
 kill $TAIL_PID
+exec 3>&-
